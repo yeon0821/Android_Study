000a00137d0932406c03380d647a5365c773feaf
Android No.1 <yeon8747@naver.com>
2022-07-05 23:41:23 +0900

    Tdo List - Custom 🏀

--- RecyclerView_TodoListIntent/app/src/main/java/org/techtown/recyclerview_todolistintent/MainActivity2.java
+++ RecyclerView_TodoListIntent/app/src/main/java/org/techtown/recyclerview_todolistintent/MainActivity2.java
@@ -9,11 +9,11 @@ import android.widget.TextView;
 
 public class MainActivity2 extends Activity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-
     }
 }

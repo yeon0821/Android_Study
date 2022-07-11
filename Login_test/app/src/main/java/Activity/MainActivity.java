fd78d85151b84c06de7d5cba53e9f71a02adc994
Android No.1 <yeon8747@naver.com>
2022-07-11 22:31:26 +0900

    🎳 MainActivity

---
+++ Login_test/app/src/main/java/Activity/MainActivity.java
@@ -0,0 +1,22 @@
+package Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.techtown.login_test.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+
+
+
+
+
+    }
+}

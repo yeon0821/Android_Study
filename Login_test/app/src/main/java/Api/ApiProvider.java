ffc5c2d537f5cb428a8fe8b3c5fe43a048d02476
Android No.1 <yeon8747@naver.com>
2022-07-11 22:32:40 +0900

    🏀 Retrofit

---
+++ Login_test/app/src/main/java/Api/ApiProvider.java
@@ -0,0 +1,19 @@
+package Api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiProvider {
+    private static Retrofit retrofit;
+    private static String BASE_URL = "http://192.168.137.168:2000/";
+
+    public static Retrofit getRetrofit(){
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}

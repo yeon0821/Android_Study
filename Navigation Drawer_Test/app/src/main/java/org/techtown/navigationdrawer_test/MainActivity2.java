96ccc80c84a5a818141998c2f3fb14d893508c8c
Android No.1 <yeon8747@naver.com>
2022-07-22 22:12:37 +0900

    Navigation Drawer_Test 🎳

---
+++ Navigation Drawer_Test/app/src/main/java/org/techtown/navigationdrawer_test/MainActivity2.java
@@ -0,0 +1,20 @@
+package org.techtown.navigationdrawer_test;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

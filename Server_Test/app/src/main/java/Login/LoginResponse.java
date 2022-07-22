c0d368f850d4c33a0b90fe418351ff8587fadd8c
Android No.1 <yeon8747@naver.com>
2022-07-22 22:13:16 +0900

    👨‍💻 Server Test(Retrofit2)

--- Server_Test/app/src/main/java/Login/LoginResponse.java
+++ Server_Test/app/src/main/java/Login/LoginResponse.java
@@ -7,6 +7,9 @@ public class LoginResponse {
     private String account_id;
     private String password;
 
+    public LoginResponse(String account_id, String password){
+    }
+
     public String getAccount_id() {
         return account_id;
     }
